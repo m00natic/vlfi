72fec35f4462c48c914e5819c1bd3cc0b247ddd1
Andrey Kotlarski <m00naticus@gmail.com>
2013-12-11 03:21:08 +0200

    Update README and bump version.

--- vlf.el
+++ vlf.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2006, 2012, 2013  Free Software Foundation, Inc.
 
-;; Version: 1.1
+;; Version: 1.2
 ;; Keywords: large files, utilities
 ;; Maintainer: Andrey Kotlarski <m00naticus@gmail.com>
 ;; Authors: 2006 Mathias Dahl <mathias.dahl@gmail.com>
